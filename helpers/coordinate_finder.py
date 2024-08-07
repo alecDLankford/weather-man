4e4520e6492b17961be858e64497ea0672d3b95f
alecDLankford <alec.d.lankford@gmail.com>
2024-08-06 19:28:58 -0500

    Linting, removing print statement

--- helpers/coordinate_finder.py
+++ helpers/coordinate_finder.py
@@ -1,14 +1,14 @@
+from typing import Tuple
 from geopy.geocoders import Nominatim
 
 geolocator = Nominatim(user_agent="coordinateFinder")
 
-def get_coordinates(city: str, state: str) -> tuple[float, float]:
+def get_coordinates(city: str, state: str) -> Tuple[float, float]:
     location = geolocator.geocode(city + ", " + state)
 
     if location:
         longitude = location.longitude
         latitude = location.latitude
-        print("lat", latitude, "long", longitude)
         return (latitude, longitude)
 
     return (None, None)
